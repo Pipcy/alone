e7ef2de391719ce3c15e7da58834edbef3742481
piporhicyan <79942890+Pipcy@users.noreply.github.com>
2023-03-09 01:49:56 -0500

    3/8

---
+++ alone/Assets/scripts/PauseMenu.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool GameIsPaused = false;
+    /*
+        to use this vaiable in other scripts:
+        directly refer to it as "PauseMenu.GameIsPaused", no need of anything else.
+
+    */
+
+    public GameObject pauseMenuUI;
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            if(GameIsPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+
+
+
+    public void Resume ()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+    }
+
+    public void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        GameIsPaused = true;
+    }
+
+    public void LoadTutorial()
+    {
+        Debug.Log("load tutolrial...");
+    }
+
+    public void LoadMenu()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("Title");
+    }
+}
--- alone/Assets/scripts/UI/sceneChange.cs
+++ alone/Assets/scripts/UI/sceneChange.cs
@@ -9,6 +9,9 @@ public class sceneChange : MonoBehaviour
     public void AloadTitle() {
         SceneManager.LoadScene("Title"); }
 
+    public void Aintro(){
+        SceneManager.LoadScene("intro"); }
+
     public void Astart(){
         SceneManager.LoadScene("SampleScene"); }
 
